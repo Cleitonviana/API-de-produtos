896e341a0856fdcb0f51ac38339b8a0cc5ab0387
Cleitonviana <cleiton_inacio@outlook.com>
2025-02-20 12:07:20 -0300

    All endpoints created

--- src/main/java/com/example/products/controller/ProductController.java
+++ src/main/java/com/example/products/controller/ProductController.java
@@ -1,19 +1,26 @@
 package com.example.products.controller;
 
-import java.awt.desktop.OpenFilesEvent;
 import java.lang.foreign.Linker.Option;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.products.dtos.ProductDto;
 import com.example.products.models.ProductModel;
 import com.example.products.repositories.ProductRepository;
+
+import jakarta.validation.Valid;
 
 @RestController
 public class ProductController {
@@ -37,7 +44,34 @@ public class ProductController {
 		
 	}
 	
+	@PostMapping("/products")
+	public ResponseEntity<ProductModel> saveProduct(@RequestBody @Valid ProductDto productDto){
+		var productModel = new ProductModel();
+		BeanUtils.copyProperties(productDto, productModel);
+		return ResponseEntity.status(HttpStatus.CREATED).body(productRepository.save(productModel));
+	}
 	
 	
+	
+	@PutMapping("/products/{id}")
+	public ResponseEntity<Object> updateProduct(@PathVariable(value="id") Long id, @RequestBody @Valid ProductDto productDto){
+		Optional<ProductModel> product0 = productRepository.findById(id);
+		if(product0.isEmpty()) {
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).body("product not found.");
+		}
+		var productModel = product0.get();
+		BeanUtils.copyProperties(productDto, productModel);
+		return ResponseEntity.status(HttpStatus.OK).body(productRepository.save(productModel));
+	}
+	
+	@DeleteMapping("/products/{id}")
+	public ResponseEntity<Object> deleteProduct(@PathVariable(value="id") Long id){
+		Optional<ProductModel> product0 = productRepository.findById(id);
+		if(product0.isEmpty()) {
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Product not found.");
+		}
+		productRepository.delete(product0.get());
+		return ResponseEntity.status(HttpStatus.OK).body("Product deleted successfully.");
+	}
 	
 }
--- src/main/java/com/example/products/models/ProductModel.java
+++ src/main/java/com/example/products/models/ProductModel.java
@@ -7,22 +7,45 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
 @Entity
 @Table(name = "tb_products")
 public class ProductModel implements Serializable {
 	private static final long serialVersionUID = 1L;
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private long id;
+	private Long id;
 	private String name;
 	private double price;
+
+	public ProductModel() {
+	}
+
+	public ProductModel(Long id, String name, double price) {
+		this.id = id;
+		this.name = name;
+		this.price = price;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+	public Long getId() {
+		return id;
+	}
 
 }
